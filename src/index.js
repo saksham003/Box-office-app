944d7b1075cc488a989b7867c0562cfb49053c6f
Saksham003 <verms3saksham@gmail.com>
2021-10-19 14:42:01 +0530

    added screenshot git

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename="/">
     <App />
   </HashRouter>,
   document.getElementById('root')
